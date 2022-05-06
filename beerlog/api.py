6e1e88b31eed907ff31186b5cce7aa2ee21acd36
gcamposl <gcamposlopes@gmail.com>
2022-05-06 19:43:35 -0300

    building api in fastapi

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,5 +1,7 @@
 from beerlog.core import get_beers_from_database
-from beerlog.serializers import BeerOut
+from beerlog.serializers import BeerIn, BeerOut
+from beerlog.database import get_session
+from beerlog.models import Beer
 from fastapi import FastAPI
 from typing import List
 
@@ -10,3 +12,16 @@ api = FastAPI(title="Beerlog")
 def list_beers():
     beers = get_beers_from_database()
     return beers
+
+
+# payload - o que mandamos de dados na nossa requisicao
+@api.post('/beers/', response_model=BeerOut)
+def add_beer(beer_in: BeerIn):
+    beer = Beer(**beer_in.dict())
+    with get_session() as session:
+        # add a beer no db
+        session.add(beer)
+        session.commit()
+        # recupera os id quando inserida a beer
+        session.refresh(beer)
+    return beer
